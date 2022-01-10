0facf51cbd289afb32b45c614d47ade387763bea
eniola4031 <agboola.eniola4031@gmail.com>
2022-01-10 10:39:52 +0100

    updated timelock

--- timelock.sol
+++ timelock.sol
@@ -1,76 +1,362 @@
-// SPDX-License-Identifier: GPL-3.0;
+/**
+ *Submitted for verification at BscScan.com on 2021-05-16
+*/
+
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+// CAUTION
+// This version of SafeMath should only be used with Solidity 0.8 or later,
+// because it relies on the compiler's built in overflow checks.
+
+/**
+ * @dev Wrappers over Solidity's arithmetic operations.
+ *
+ * NOTE: `SafeMath` is no longer needed starting with Solidity 0.8. The compiler
+ * now has built in overflow checking.
+ */
+library SafeMath {
+    /**
+     * @dev Returns the addition of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryAdd(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            uint256 c = a + b;
+            if (c < a) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    /**
+     * @dev Returns the substraction of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function trySub(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b > a) return (false, 0);
+            return (true, a - b);
+        }
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, with an overflow flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMul(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            // Gas optimization: this is cheaper than requiring 'a' not being zero, but the
+            // benefit is lost if 'b' is also tested.
+            // See: https://github.com/OpenZeppelin/openzeppelin-contracts/pull/522
+            if (a == 0) return (true, 0);
+            uint256 c = a * b;
+            if (c / a != b) return (false, 0);
+            return (true, c);
+        }
+    }
+
+    /**
+     * @dev Returns the division of two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryDiv(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a / b);
+        }
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers, with a division by zero flag.
+     *
+     * _Available since v3.4._
+     */
+    function tryMod(uint256 a, uint256 b) internal pure returns (bool, uint256) {
+        unchecked {
+            if (b == 0) return (false, 0);
+            return (true, a % b);
+        }
+    }
+
+    /**
+     * @dev Returns the addition of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `+` operator.
+     *
+     * Requirements:
+     *
+     * - Addition cannot overflow.
+     */
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a + b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting on
+     * overflow (when the result is negative).
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a - b;
+    }
+
+    /**
+     * @dev Returns the multiplication of two unsigned integers, reverting on
+     * overflow.
+     *
+     * Counterpart to Solidity's `*` operator.
+     *
+     * Requirements:
+     *
+     * - Multiplication cannot overflow.
+     */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a * b;
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `/` operator.
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a / b;
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting when dividing by zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a % b;
+    }
+
+    /**
+     * @dev Returns the subtraction of two unsigned integers, reverting with custom message on
+     * overflow (when the result is negative).
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {trySub}.
+     *
+     * Counterpart to Solidity's `-` operator.
+     *
+     * Requirements:
+     *
+     * - Subtraction cannot overflow.
+     */
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        unchecked {
+            require(b <= a, errorMessage);
+            return a - b;
+        }
+    }
+
+    /**
+     * @dev Returns the integer division of two unsigned integers, reverting with custom message on
+     * division by zero. The result is rounded towards zero.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Counterpart to Solidity's `/` operator. Note: this function uses a
+     * `revert` opcode (which leaves remaining gas untouched) while Solidity
+     * uses an invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a / b;
+        }
+    }
+
+    /**
+     * @dev Returns the remainder of dividing two unsigned integers. (unsigned integer modulo),
+     * reverting with custom message when dividing by zero.
+     *
+     * CAUTION: This function is deprecated because it requires allocating memory for the error
+     * message unnecessarily. For custom revert reasons use {tryMod}.
+     *
+     * Counterpart to Solidity's `%` operator. This function uses a `revert`
+     * opcode (which leaves remaining gas untouched) while Solidity uses an
+     * invalid opcode to revert (consuming all remaining gas).
+     *
+     * Requirements:
+     *
+     * - The divisor cannot be zero.
+     */
+    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        unchecked {
+            require(b > 0, errorMessage);
+            return a % b;
+        }
+    }
+}
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
 
-contract TimeLockedWallet {
-    address public creator;
+pragma solidity ^0.8.0;
+
+contract TokenLockup {
+
+    using SafeMath for uint256;
+    // NO DECIMAL NUMBERS IN ANY INPUT (Not Supported by solidity directly)
     address public owner;
-    uint256 public unlockDate = 365 days;
-    uint256 public createdAt;
-    address public constant presaleToken = "0x68E374F856bF25468D365E539b700b648Bf94B67"; // Using DAI from testnet, replace this with real token address CHANGE THIS
+    
+    address public constant token = 0x68E374F856bF25468D365E539b700b648Bf94B67; // Using DAI from testnet, replace this with real token address CHANGE THIS
+    
+    uint256 public constant tokenDecimal = 18; // Token Decimals CHANGE THIS
+    
+    uint256 private constant decimalFactor = 10 ** uint256(tokenDecimal);
+    
+    uint256 public constant distributionAmount = 1250000; // Amount of tokens to be distributed to each person per Distribution CHANGE THIS
+    
+    uint256 public constant initialDelay = 12 * 30 days; // Total days before the unlocking starts, edit to change number of months CHANGE FIRST NUMBER ONLY
+    // Example 1 * 30 days = 1 month, 2 * 30 days = 2 months etc.
+    
+    uint256 public nextUnlock = block.timestamp + initialDelay;
+    
+    uint256 public constant interval = 1 * 30 days;  // Delays between each unlock CHANGE FIRST NUMBER ONLY
+    // Example: Similar to initialDelay
+    
+    uint256 public constant cycles = 12 ; // Numbers of months the distribution needs to run for CHANGE THIS
+    
+    uint256 public unlockEnd = block.timestamp + initialDelay + (cycles * interval);
+    
+    uint256 public constant scaledAmount = distributionAmount * decimalFactor;
+    
+    IERC20 _token = IERC20(token);
+    
+    address[] public whiteList = [
+        0x9AFFe73C2716A4F5F424909750c9Dc21a4D9Da07,
+        0x32Ba9D95323f661CB61867143cCC042aFC98bB06,
+        0xf9938e84c53Dcdc4Bcd6479C8f6c6C94119e3EAc,
+        0xb6A6319b5712DD535FA506d6CC9F8e45a5D9F792,
+        0x3B0498Ef73db974d53Cfd8168Aa47e002b24b8d2,
+        0x85d485Be26D881F3a7f0cCAeCBe2cE79F92b3f49
+        ]; // CHANGE THIS to proper whiteList
 
-    event Received(address _from, uint _amount);
-    event Withdrew(address _to, uint _amount);
-    event WithdrewTokens(address _tokenContract, address _to, uint _amount);
-    
-    modifier onlyOwner {
-        require(msg.sender == owner);
-            _;
+    constructor() {
+        owner = 0x20bA5eD762E521d5745bC69EbAe92cE5209daC97; // CHANGE THIS to whoever is expected to recieve leftover tokens (This is probably the main dev/token minter's wallet)
     }
-    receive() external payable{
-        emit Received(msg.sender, msg.value);
-    }
-    constructor (address _creator, address _owner /*uint _unlockDate*/) {
-        creator = _creator;
-        owner = _owner;
-       // unlockDate = _unlockDate;
-        createdAt = block.timestamp;
-    }
-    
-    function info() public view returns(address, address, uint, uint) {
-        return (creator, owner, unlockDate, createdAt);
-    }
-    //withdraw from contract to msg.sender 
-    function withdraw() onlyOwner public {
-        require(block.timestamp >= unlockDate);
-        payable(msg.sender).transfer(address(this).balance);
-        emit Withdrew(msg.sender, address(this).balance);
-    }
-    
-    function withdrawTokens(address _tokenContract) onlyOwner public {
-        require(_tokenContract != address(0), "Token not found");
-        require(block.timestamp >= unlockDate);
-        IERC20 token = IERC20(_tokenContract);
-        uint tokenBalance = token.balanceOf(address(this));
-        token.transfer(msg.sender, tokenBalance);
-       emit WithdrewTokens(_tokenContract, msg.sender, tokenBalance);
-    }
-    
-    function deposit(address _tokenContract, uint _amount) public {
-        require(_amount > 0, "invalid value");
-        IERC20 token = IERC20(_tokenContract);
-        token.transferFrom(msg.sender, address(this), _amount);
-    }
-    
-    function balanceOf(address _tokenContract) public view returns(uint){
-        IERC20 token = IERC20(_tokenContract);
-        uint tokenBalance = token.balanceOf(address(this));
-        return tokenBalance;
-    }
-         function checkUpkeep(bytes calldata /* checkData */) external override view returns (bool /* upkeepNeeded */, bytes memory /*performData*/) {
-        bool upkeepNeeded;
-        upkeepNeeded = block.timestamp >= unlockDate;
-        return (upkeepNeeded, bytes(""));
-    }
-    
-    function performUpkeep(bytes calldata /* performData */) external override {
-        lastTimeStamp = block.timestamp;
-        withdraw();  
-        withdrawToken(presaleToken);
-          }
 
-
+    function distribute() public // This can be called by anyone and will distribute the tokens to everyone on the whitelist, as long as Distribution is possible
+    {
+        require(block.timestamp > nextUnlock, "No tokens to distribute yet");
+        require(unlockEnd > nextUnlock, "Distribution Completed"); 
+        nextUnlock = nextUnlock + interval;
+        for (uint256 i = 0; i < whiteList.length; i++) {
+            _token.transfer(whiteList[i], scaledAmount);
+        }
+    }
+    
+    function recoverLeftover() public // Only callable once distribute has been called X amount of times (sucessfully), where X is the number of cycles above
+    {
+        require(unlockEnd <= nextUnlock, "Distribution not completed");
+        uint256 leftOverBalance = _token.balanceOf(address(this));
+        _token.transfer(owner, leftOverBalance);
+    }
 }
